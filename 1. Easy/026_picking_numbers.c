08e31482a2f55681e33dfa9957ba896526c14f3e
Md. Rifat <jahidulislamrefat6@gmail.com>
2025-12-28 12:35:31 +0600

    Add solution to Picking Numbers problem

--- 1. Easy/026_picking_numbers.c
+++ 1. Easy/026_picking_numbers.c
@@ -1,3 +1,9 @@
+// Problem: Picking Numbers
+// link: https://www.hackerrank.com/challenges/picking-numbers/problem
+// Approach: Count the frequency of each number. For each value x, calculate freq[x] + freq[x+1].
+//           The maximum of these sums is the longest valid subarray.
+
+
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -13,15 +19,15 @@ int main(){
 
     int freq[101] = {0};
 
-    for (int i = 0; i < size; i++){ 
+    for (int i = 0; i < size; i++){                     // Counts the frequency for each number and stores them in array.
         freq[arr[i]]++;
-    }
+    }   
 
     int maxLen = 0;
     for (int i = 0; i < 100; i++){
-        int current = freq[i] + freq[i + 1];
+        int current = freq[i] + freq[i + 1];           // Counts the element number of each sub array that meets the criteria    
 
-        if (current > maxLen){
+        if (current > maxLen){                         // Gets the highest element number
             maxLen = current;
         }
     }
